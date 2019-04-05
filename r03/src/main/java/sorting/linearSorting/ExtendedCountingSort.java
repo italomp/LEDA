fa20b7b82a21633294ea22077cbafd138384c407
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2019-04-05 17:22:12 -0300

    Counting otimizado (poupando memória no array aux)

--- r03/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ r03/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -6,7 +6,7 @@ import sorting.AbstractSorting;
  * Classe que implementa do Counting Sort vista em sala. Desta vez este
  * algoritmo deve satisfazer os seguitnes requisitos: - Alocar o tamanho minimo
  * possivel para o array de contadores (C) - Ser capaz de ordenar arrays
- * contendo numeros negativos
+ * contendo numeros negativos.
  */
 public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
