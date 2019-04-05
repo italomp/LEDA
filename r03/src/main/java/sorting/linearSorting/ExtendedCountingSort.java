969fb5b97ba20929c4879834aebb3547a2e5bd4c
Italo Modesto <italo.pereira@ccc.ufcg.edu.br>
2019-04-04 23:26:27 -0300

    Counting otimizado (poupando memória no array aux)

--- r03/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ r03/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,69 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(array.equals(null)) {
+			return;
+		}
+		if(leftIndex < 0) {
+			leftIndex = 0;
+		}
+		if(rightIndex > array.length -1) {
+			rightIndex = array.length -1;
+		}
+		if(leftIndex > rightIndex) {
+			return;
+		}
+		int maior = descobreMaior(array);
+		int menor = descobreMenor(array);
+		Integer[] aux = new Integer[maior - menor + 1];
+		Integer[] saida = new Integer[array.length];
+		
+		for(int i= 0; i < aux.length; i++) {
+			aux[i] = 0;
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			aux[array[i] - menor]++;
+		}
+		
+		for(int i = 1; i < aux.length; i++) {
+			aux[i] += aux[i - 1];
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			saida[aux[array[i] - menor]-- - 1] = array[i];
+		}
+		
+		for(int i = 0; i < array.length; i++) {
+			array[i] = saida[i];
+		}
+		
+	}
+	
+	public Integer descobreMaior(Integer[] array) {
+		if(array.length >= 1) {
+			int maior = array[0];
+			for(int i = 0; i < array.length; i++) {
+				if(array[i] > maior) {
+					maior = array[i];
+				}
+			}
+			return maior;
+		}
+		return null;
+	}
+	
+	public Integer descobreMenor(Integer[] array) {
+		if(array.length >= 1) {
+			int menor = array[0];
+			for(int i = 0; i < array.length; i++) {
+				if(array[i] < menor) {
+					menor = array[i];
+				}
+			}
+			return menor;
+		}
+		return null;
 	}
 
 }
